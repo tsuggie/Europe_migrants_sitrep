580c78765f41d2120803b120cb28dd3b75e59bfd
Heidi El-Hosaini <heidi.elhosaini@gmail.com>
2015-11-13 18:28:41 +0000

    Amended comments

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
-/* NEXT:
+/* NEXT CHANGES:
 	- row headings - right justify, horizontal highlight lines more obvious? - needs to be done in library
 	- objectify stats generation
 	- *** add shapefile for locations - Simon to update library first - need qgis to accept utf-8
